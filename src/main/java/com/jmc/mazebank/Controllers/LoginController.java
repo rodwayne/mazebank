568508354e18b42e621a3250489945981f7af11c
rodwayne <53350291+rodwayne@users.noreply.github.com>
2024-09-29 20:47:07 -0500

    Add control properties to LoginController

--- src/main/java/com/jmc/mazebank/Controllers/LoginController.java
+++ src/main/java/com/jmc/mazebank/Controllers/LoginController.java
@@ -1,4 +1,15 @@
 package com.jmc.mazebank.Controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public ChoiceBox account_selector;
+    public Label payee_address;
+    public TextField payee_address_field;
+    public TextField password_field;
+    public Button login_button;
+    public Label error_label;
 }
